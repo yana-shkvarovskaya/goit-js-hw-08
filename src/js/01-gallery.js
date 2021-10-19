b6b37fb40b3c0fbbe5c51c9c2065ee69845793db
yana-shkvarovskaya <shkvarovskayayana@gmail.com>
2021-10-19 17:50:42 +0300

    настраивает parcel и делает первое задание

---
+++ src/js/01-gallery.js
@@ -0,0 +1,29 @@
+// // // Add imports above this line
+import { galleryItems } from './gallery-items';
+// // // Change code below this line
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryImgsMarkup = createGalleryImgMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryImgsMarkup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function createGalleryImgMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href=${original}>
+        <img class="gallery__image" src=${preview} alt=${description} />
+        </a> 
+      `;
+    })
+    .join("");
+}
